ce2f6583fae5b781709a7ca5cf29d5a612396984
Artem Novok <aan200545@gmail.com>
2024-06-23 22:29:49 -0700

    basic store logic

--- store.go
+++ store.go
@@ -1,38 +1,123 @@
 package main
 
 import (
+	"bytes"
+	"crypto/sha1"
+	"encoding/hex"
+	"errors"
 	"fmt"
 	"io"
+	"io/fs"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
-type PathTransformFunc func(string) string
+// PathTransFormFunc is a function that generate PathKey from the given key string
+type PathTransformFunc func(string) PathKey
 
+// PathKey represents the path to the file and the file name
+type PathKey struct {
+	PathName string
+	FileName string
+}
+
+// FullPath name returns full path to the file
+func (p *PathKey) FullPath() string {
+	return filepath.Join(p.PathName, p.FileName)
+}
+
+// RootPathDIr returns root directory of the path to the file
+func (p *PathKey) RootPathDir() string {
+	paths := strings.Split(p.PathName, "/")
+	if len(paths) == 0 {
+		return ""
+	}
+	return paths[0]
+}
+
+// CASPathTransformFunc return PathKey struct generated from given key
+func CASPathTransformFunc(key string) PathKey {
+	hash := sha1.Sum([]byte(key))
+	hashStr := hex.EncodeToString(hash[:])
+	blockSize := 5
+	sliceLen := len(hashStr) / blockSize
+	paths := make([]string, sliceLen)
+	for i := 0; i < sliceLen; i++ {
+		from, to := i*blockSize, i*blockSize+blockSize
+		paths[i] = hashStr[from:to]
+	}
+
+	return PathKey{
+		PathName: strings.Join(paths, "/"),
+		FileName: hashStr,
+	}
+}
+
+// StoreOpts are options for store struct
 type StoreOpts struct {
 	PathTransformFunc PathTransformFunc
 }
 
+// Store represents the struct responsible for storing and managing data on the lowest level
 type Store struct {
 	StoreOpts
 }
 
+// NewStore generate new store with given options
 func NewStore(opts StoreOpts) *Store {
 	return &Store{
 		StoreOpts: opts,
 	}
 }
+
 func DefaultPathTransformFunc(key string) string {
 	return key
 }
+
+// Has returns true if given key is exists or false if doesn't
+func (s *Store) Has(key string) bool {
+	pathKey := s.PathTransformFunc(key)
+	_, err := os.Stat(pathKey.FullPath())
+	return !errors.Is(err, fs.ErrNotExist)
+}
+
+// Delete deletes data and all path for given key
+func (s *Store) Delete(key string) error {
+	pathKey := s.PathTransformFunc(key)
+	return os.RemoveAll(pathKey.RootPathDir())
+}
+
+// Read returns io.Reader for further logic with out need to close file
+func (s *Store) Read(key string) (io.Reader, error) {
+	f, err := s.readStream(key)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	buf := new(bytes.Buffer)
+	_, err = io.Copy(buf, f)
+	if err != nil {
+		return nil, err
+	}
+	return buf, nil
+}
+
+// readStream returns file stored at given key
+func (s *Store) readStream(key string) (io.ReadCloser, error) {
+	pathKey := s.PathTransformFunc(key)
+	return os.Open(pathKey.FullPath())
+}
+
+// writeStream writes data from io.Reader and stores it at path generated from given key
 func (s *Store) writeStream(key string, r io.Reader) error {
 	pathName := s.PathTransformFunc(key)
-	if err := os.MkdirAll(pathName, os.ModePerm); err != nil {
+	if err := os.MkdirAll(pathName.PathName, os.ModePerm); err != nil {
 		return err
 	}
-	fileName := "someFileName"
-	pathToFile := filepath.Join(pathName, fileName)
-	f, err := os.Create(pathToFile)
+
+	pathAndFileName := pathName.FullPath()
+	f, err := os.Create(pathAndFileName)
 	if err != nil {
 		return nil
 	}
@@ -40,6 +125,6 @@ func (s *Store) writeStream(key string, r io.Reader) error {
 	if err != nil {
 		return err
 	}
-	fmt.Printf("written (%d) bytes to disk: %s", n, pathToFile)
+	fmt.Printf("written (%d) bytes to disk: %s", n, pathAndFileName)
 	return nil
 }
--- store_test.go
+++ store_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"io"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -9,21 +10,33 @@ import (
 
 func Test_NewSore(t *testing.T) {
 	opts := StoreOpts{
-		PathTransformFunc: DefaultPathTransformFunc,
+		PathTransformFunc: CASPathTransformFunc,
 	}
 	store := NewStore(opts)
 	key := "key"
+	wantPath := "a62f2/225bf/70bfa/ccbc7/f1ef2/a3978/36717/377de"
+	wantOrigin := "a62f2225bf70bfaccbc7f1ef2a397836717377de"
 	tr := store.PathTransformFunc(key)
-	require.Equal(t, tr, key)
+	require.Equal(t, tr.PathName, wantPath)
+	require.Equal(t, tr.FileName, wantOrigin)
 }
 
-func Test_writeStream(t *testing.T) {
+func Test_writeStream_Has_Delete(t *testing.T) {
 	opts := StoreOpts{
-		PathTransformFunc: DefaultPathTransformFunc,
+		PathTransformFunc: CASPathTransformFunc,
 	}
 	store := NewStore(opts)
-	key := "specialKey"
-	payLoad := bytes.NewReader([]byte("some jpg bytes"))
+	key := "fatcatpicture"
+	data := "some jpg bytes"
+	payLoad := bytes.NewReader([]byte(data))
 	require.Nil(t, store.writeStream(key, payLoad))
-
+	bytes, err := store.Read(key)
+	require.Nil(t, err)
+	src, err := io.ReadAll(bytes)
+	require.Nil(t, err)
+	require.Equal(t, string(src), data)
+	exists := store.Has(key)
+	require.Equal(t, exists, true)
+	err = store.Delete(key)
+	require.Nil(t, err)
 }
