d4fa81bc311ea9707c524875e2568e427efb590d
Artem Novok <aan200545@gmail.com>
2024-06-27 16:01:21 -0700

    added server test

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -1,4 +1,4 @@
-package main
+package server
 
 import (
 	"bytes"
@@ -6,27 +6,26 @@ import (
 	"fmt"
 	"gofilesystem/internal/logger/mylogger"
 	"gofilesystem/internal/p2p"
-	"gofilesystem/internal/server"
 	"gofilesystem/internal/store"
 	"io"
 	"log"
 	"log/slog"
 	"os"
+	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 const (
-	defaultLoggerLevel = "DEBUG"
+	defaultLoggerLevel = "DEV"
 )
 
-func OnPeer(p p2p.Peer) error {
-	fmt.Println("execute OnPeer function")
-	p.Close()
-	return nil
-}
-func main() {
-	gob.Register(server.MessageStoreFile{})
-	gob.Register(server.MessageGetFile{})
+// This test tests main functions of the server
+func Test_Server(t *testing.T) {
+	gob.Register(MessageStoreFile{})
+	gob.Register(MessageGetFile{})
 	logger := setUpLogger()
 	s := makeServer(":3000", "3000", logger)
 	s2 := makeServer(":4000", "4000", logger, ":3000")
@@ -36,15 +35,19 @@ func main() {
 	go s2.Start()
 	time.Sleep(2 * time.Second)
 	start := time.Now()
-	for i := 0; i < 450; i++ {
+	// Create keys for one server and than it will create them for other
+	for i := 0; i < 45; i++ {
 		key := fmt.Sprintf("mydata%v", i)
 		payload := fmt.Sprintf("so much data %v", i)
 		data := bytes.NewReader([]byte(payload))
 		s2.StoreData(key, data)
 		time.Sleep(5 * time.Millisecond)
 	}
+	// delete one server data and check if data is actually removed
 	s2.Store.Clear()
-	for i := 0; i < 450; i++ {
+	require.Error(t, os.Chdir("4000"))
+	// Start requesting deleting keys to force server to look up for them in other servers
+	for i := 0; i < 45; i++ {
 		key := fmt.Sprintf("mydata%v", i)
 		_, r, err := s2.Get(key)
 		if err != nil {
@@ -56,14 +59,30 @@ func main() {
 		}
 		fmt.Println(string(b))
 	}
-	// _, err = s.Get("mydata")
-	// if err != nil {
-	// 	logger.Error("got error", slog.String("error", err.Error()))
-	// }
+	// Make sure that both servers has all files and that files are equal
+	for i := 0; i < 45; i++ {
+		key := fmt.Sprintf("mydata%v", i)
+		n2, r2, err := s2.Get(key)
+		require.Nil(t, err)
+		n, r, err := s.Get(key)
+		require.Nil(t, err)
+		assert.Equal(t, n, n2)
+		b2, err := io.ReadAll(r2)
+		require.Nil(t, err)
+		b, err := io.ReadAll(r)
+		require.Nil(t, err)
+		require.Equal(t, b, b2)
+	}
+	// Removing all data from servers and making sure that data is actually removed
+	s2.Store.Clear()
+	s.Store.Clear()
+	require.Error(t, os.Chdir("3000"))
+	require.Error(t, os.Chdir("4000"))
 	logger.Info("done")
 	logger.Info("took", slog.Duration("time", time.Duration(time.Since(start).Seconds())))
 }
-func makeServer(Addr, root string, logger *slog.Logger, nodes ...string) *server.FileServer {
+
+func makeServer(Addr, root string, logger *slog.Logger, nodes ...string) *FileServer {
 	tcpTrOpts := p2p.TCPTransportOpts{
 		ListenerAddress: Addr,
 		ShakeHandsFunc:  p2p.NoHandshakeFunc,
@@ -71,14 +90,14 @@ func makeServer(Addr, root string, logger *slog.Logger, nodes ...string) *server
 		Log:             logger,
 	}
 	tcpTransport := p2p.NewTCPTransport(tcpTrOpts)
-	serverOpts := server.FileServerOpts{
+	serverOpts := FileServerOpts{
 		StorageRoot:       root,
 		PathTransformFunc: store.CASPathTransformFunc,
 		Transport:         tcpTransport,
 		Log:               logger,
 		BootstrapNodes:    nodes,
 	}
-	s := server.NewFileServer(serverOpts)
+	s := NewFileServer(serverOpts)
 	tcpTransport.OnPeer = s.OnPeer
 	return s
 }
